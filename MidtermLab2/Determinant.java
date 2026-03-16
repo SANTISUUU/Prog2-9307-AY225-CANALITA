5eab5f16a03e1ffcf8d5d0b5bd86654749786cd2
Chino <nochiatilanac@gmail.com>
2026-03-16 10:30:45 +0800

    Added Determinant.java

---
+++ MidtermLab2/Determinant.java
@@ -0,0 +1,10 @@
+public class Determinant {
+    public static void main(String[] args) {
+
+        int det = 5*(4*(-2) - 1*(-1))
+                - 2*((-1)*(-2) - 1*2)
+                + (-3)*((-1)*(-1) - 4*2);
+
+        System.out.println(det);
+    }
+}
